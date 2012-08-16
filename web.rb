86c42f2990eabade2d733910b558c7c7e9e04fbd
Hal Seki <hal@plants-web.jp>
2012-08-16 15:23:49 +0900

    implemented dictionary searching

--- web.rb
+++ web.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'yaml'
+require 'json'
 
 def get_or_post(path, opts={}, &block)
     get(path, opts, &block)
@@ -7,5 +9,18 @@ end
 
 get_or_post '/' do
   content_type 'text/plain', :charset => 'utf-8'
-  "Hello, world!"
+  dict = YAML::load( File.open( 'dict.yaml' ) )
+  dict['hitachi'].default = ''
+  dict['reinspire'].default = ''
+  begin
+    json = JSON.parse(request.body.string)
+  rescue
+    json = JSON.parse( File.read( 'test.json' ) )
+  end
+  ret = ''
+  json["events"].map{ |e|
+    ret += dict['hitachi'][e["message"]["text"]] + "\n" if dict['hitachi'][e["message"]["text"]] != ''
+    ret += dict['reinspire'][e["message"]["text"]] + "\n" if dict['reinspire'][e["message"]["text"]] != ''
+  }
+  ret
 end
