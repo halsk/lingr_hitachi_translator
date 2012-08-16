71d39a2106ac029ba79615d984407a2b1d71eb65
TAKANO Mitsuhiro <takano32@gmail.com>
2012-08-16 16:12:26 +0900

    refactor and indent web.rb

--- web.rb
+++ web.rb
@@ -3,24 +3,31 @@ require 'yaml'
 require 'json'
 
 def get_or_post(path, opts={}, &block)
-    get(path, opts, &block)
-      post(path, opts, &block)
+  get(path, opts, &block)
+  post(path, opts, &block)
 end
 
 get_or_post '/' do
   content_type 'text/plain', :charset => 'utf-8'
-  dict = YAML::load( File.open( 'dict.yaml' ) )
+
+  dict = YAML::load File.open('dict.yaml')
   dict['hitachi'].default = ''
   dict['reinspire'].default = ''
+
   begin
-    json = JSON.parse(request.body.string)
+    json = JSON.parse request.body.string
   rescue
-    json = JSON.parse( File.read( 'test.json' ) )
+    json = JSON.parse File.read('test.json')
   end
-  ret = ''
-  json["events"].map{ |e|
-    ret += dict['hitachi'][e["message"]["text"]] + "\n" if dict['hitachi'][e["message"]["text"]] != ''
-    ret += dict['reinspire'][e["message"]["text"]] + "\n" if dict['reinspire'][e["message"]["text"]] != ''
+
+  lines = []
+  json['events'].map{ |e|
+    msg = dict['hitachi'][e['message']['text']]
+    rmsg = dict['reinspire'][e['message']['text']]
+    lines << msg unless msg.empty?
+    lines << rmsg unless rmsg.empty?
   }
-  ret
+
+  ret = lines.join "\n"
 end
+
