3a8c6f2be98d549f158c0ddb70b138cdc110ec6b
Hal Seki <hal@plants-web.jp>
2012-08-16 14:32:04 +0900

    changed to accept post

--- web.rb
+++ web.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
 
-get '/' do
+def get_or_post(path, opts={}, &block)
+    get(path, opts, &block)
+      post(path, opts, &block)
+end
+
+get_or_post '/' do
   content_type 'text/plain', :charset => 'utf-8'
   "Hello, world!"
 end
